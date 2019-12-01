ac69ecaf40387280404c7712591958f2aeef0092
Armin Belsner <armin.belsner@gmail.com>
2019-12-01 11:30:02 +0100

    Day1 of AoC2019

---
+++ Day1/python/AoC_Day1Solution1.py
@@ -0,0 +1,11 @@
+zahlen = []
+
+with open("../Day1Input.txt") as f:
+    for zeile in f:
+        zahlen.append(int(zeile.strip()))
+mysum = 0
+for zahl in zahlen:
+    res = zahl // 3 - 2
+    mysum += res
+
+print(mysum)
---
+++ Day1/python/AoC_Day1Solution2.py
@@ -0,0 +1,17 @@
+zahlen = []
+testinput  = 100756
+def calculate (zahlen):
+    mysum = 0
+    for zahl in zahlen:
+        res = zahl // 3 - 2
+        while 
+        mysum += res
+    return mysum
+
+
+with open("../Day1Input.txt") as f:
+    for zeile in f:
+        zahlen.append(int(zeile.strip()))
+
+
+print(calculate(zahlen))
